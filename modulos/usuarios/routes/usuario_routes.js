0d332ba2d7833cac608af0f6aef1c9d5e6bcaa75
sara <procesosprocesosaguasol@gmail.com>
2026-05-22 13:50:38 -0500

    permisos

--- modulos/usuarios/routes/usuario_routes.js
+++ modulos/usuarios/routes/usuario_routes.js
@@ -3,12 +3,18 @@ import { getUsuarios,getResumenUsuarios,putUsuarios,patchDatosUsuario,deleteUsua
 import { requirePermission } from '../../../middlewares/authmiddleware.js'
 
 const usuarioRoutes = express.Router()
+const PERMISOS_USUARIO = {
+    ver: 'usuario.ver',
+    crear: 'usuario.crear',
+    editar: 'usuario.editar',
+    eliminar: 'usuario.eliminar'
+}
 
-usuarioRoutes.get('/resumen-estados', requirePermission('usuario.ver'), getResumenUsuarios)
-usuarioRoutes.get('/', requirePermission('usuario.ver'), getUsuarios)
-usuarioRoutes.patch('/:id/datos', requirePermission('usuario.editar'), patchDatosUsuario)
-usuarioRoutes.put('/:id', requirePermission('usuario.editar'), putUsuarios)
-usuarioRoutes.post('/', requirePermission('usuario.crear'), postUsuarios)
-usuarioRoutes.delete('/:id', requirePermission('usuario.eliminar'), deleteUsuarios)
+usuarioRoutes.get('/resumen-estados', requirePermission(PERMISOS_USUARIO.ver), getResumenUsuarios)
+usuarioRoutes.get('/', requirePermission(PERMISOS_USUARIO.ver), getUsuarios)
+usuarioRoutes.patch('/:id/datos', requirePermission(PERMISOS_USUARIO.editar), patchDatosUsuario)
+usuarioRoutes.put('/:id', requirePermission(PERMISOS_USUARIO.editar), putUsuarios)
+usuarioRoutes.post('/', requirePermission(PERMISOS_USUARIO.crear), postUsuarios)
+usuarioRoutes.delete('/:id', requirePermission(PERMISOS_USUARIO.eliminar), deleteUsuarios)
 
 export default usuarioRoutes
